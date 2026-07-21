fbc4582e7dfd34bd249f98eec72d4ac49043f879
Mrudul Jade <mruduljade0@gmail.com>
2026-07-22 02:41:38 +0530

    mrudul :: splash screen and login page design

---
+++ app/src/main/java/com/mrudul/ems_admin/activities/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.mrudul.ems_admin.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mrudul.ems_admin.R;
+
+public class SplashScreen extends AppCompatActivity {
+    private final int DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },DELAY);
+    }
+}
